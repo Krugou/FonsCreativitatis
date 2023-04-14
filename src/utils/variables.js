3eab909caaab221488a1e40b0c4c432a7a504a1e
Krugou <59641229+Krugou@users.noreply.github.com>
2023-04-14 22:03:12 +0300

    alustus

---
+++ src/utils/variables.js
@@ -0,0 +1,6 @@
+const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
+const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+const appID = 'Mastersoftheuniverse';
+
+export { appID, baseUrl, mediaUrl };
+
