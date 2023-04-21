d1f561d92b2b08ebe05af87938ae6ce31b5d87fd
Krugou <59641229+Krugou@users.noreply.github.com>
2023-04-21 19:46:18 +0300

    css --> scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import {MediaProvider} from './contexts/MediaContext';
 import Home from './views/Home';
 import Layout from './views/Layout';
