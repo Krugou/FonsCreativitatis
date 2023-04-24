5979d22db5a822582f5210f50ac24ad6739ab8f4
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-04-24 12:18:21 +0300

    Homepage minifilter

--- src/components/HomeTable.jsx
+++ src/components/HomeTable.jsx
@@ -1,4 +1,11 @@
-import {Box, ImageList, Typography} from '@mui/material';
+import {
+  Box,
+  FormControl,
+  ImageList,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
 import PropTypes from 'prop-types';
 import React, {useEffect, useState} from 'react';
 import {useAuthentication, useMedia} from '../hooks/ApiHooks';
@@ -10,6 +17,7 @@ const HomeTable = ({myFilesOnly = false}) => {
   const windowSize = useWindowSize();
   const {postLogin} = useAuthentication();
   const [token, setToken] = useState(null);
+  const [sortOption, setSortOption] = useState('Latest');
 
   useEffect(() => {
     const fetchDefaultUserToken = async () => {
@@ -19,12 +27,30 @@ const HomeTable = ({myFilesOnly = false}) => {
     };
     fetchDefaultUserToken();
   }, [postLogin]);
-
+  const handleChange = (event) => {
+    setSortOption(event.target.value);
+  };
   return (
     <>
-      <Typography variant="h3" component="h2" sx={{mt: 4}}>
-        Latest Reviews:
-      </Typography>
+      <Box
+        sx={{
+          mt: 4,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h3" component="h2">
+          {sortOption} Reviews:
+        </Typography>
+        <FormControl>
+          <Select value={sortOption} onChange={handleChange}>
+            <MenuItem value="Latest">Latest</MenuItem>
+            <MenuItem value="Oldest">Oldest</MenuItem>
+            <MenuItem value="Most-liked">Most Liked</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
       <ImageList
         cols={windowSize.width > 768 ? 4 : 2}
         gap={24}
