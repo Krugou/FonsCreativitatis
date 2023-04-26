8b6844c59a0f92b844eee991e170edccf9198490
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-04-26 16:44:09 +0300

    profiilin editointi aloitettu

--- src/hooks/FormHooks.js
+++ src/hooks/FormHooks.js
@@ -3,6 +3,7 @@ import {useState} from 'react';
 const useForm = (callback, initState) => {
   // tekee alussa staten, jonka alkuarvo on initState
   const [inputs, setInputs] = useState(initState);
+  console.log(inputs);
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,21 +1,26 @@
-import {Avatar, Box} from '@mui/material';
+import {Avatar, Box, Button, Modal, TextField} from '@mui/material';
 import React, {useContext, useEffect, useState} from 'react';
 import HeroImage from '../components/HeroImage';
 import {MediaContext} from '../contexts/MediaContext';
 import {useTags} from '../hooks/apiHooks';
 import {mediaUrl} from '../utils/variables';
+import useForm from '../hooks/FormHooks';
 
 const Profile = () => {
   const {user} = useContext(MediaContext);
   const {getTag} = useTags();
+  const [file, setFile] = useState(null);
 
   const [avatar, setAvatar] = useState('https://placekitten.com/300');
+  const [selectedImage, setSelectedImage] = useState(avatar);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const getProfilePic = async () => {
     try {
       if (user) {
         const file = await getTag('avatar_' + user.user_id);
         setAvatar(mediaUrl + file.pop().filename);
+        setSelectedImage(mediaUrl + file.pop().filename);
       }
     } catch (error) {
       console.error(error.message);
@@ -25,6 +30,32 @@ const Profile = () => {
   useEffect(() => {
     getProfilePic();
   }, [user]); // jos taulukko tyhjä, ajetaan vain kerran
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const doUpload = () => {
+    alert('submitattu');
+    handleModalClose();
+  };
+  const {inputs, handleSubmit, handleInputChange} = useForm(doUpload);
+
+  const handleFileChange = (event) => {
+    console.log(event.target.files);
+    event.persist();
+    setFile(event.target.files[0]);
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      setSelectedImage(reader.result);
+    });
+    reader.readAsDataURL(event.target.files[0]);
+  };
+
   return (
     <>
       <HeroImage heroText="Profile" />
@@ -52,8 +83,78 @@ const Profile = () => {
               {/* if user has full name show it if not dont */}
               {user.full_name && <p>Full name: {user.full_name}</p>}
               <p>Email: {user.email}</p>
+              <Button variant="contained" onClick={handleModalOpen}>
+                Edit profile
+              </Button>
             </Box>
           </div>
+          <Modal open={isModalOpen} onClose={handleModalClose}>
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                bgcolor: 'background.paper',
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <h2>Edit Profile</h2>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="Username"
+                  name="username"
+                  value={inputs?.username || ''}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Full Name"
+                  name="full_name"
+                  value={inputs?.full_name || ''}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Email"
+                  name="email"
+                  value={inputs?.email || ''}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                />
+                {avatar && (
+                  <Avatar
+                    src={selectedImage}
+                    imgProps={{alt: `${user.username}'s profile picture`}}
+                    sx={{width: '5rem', height: '5rem'}}
+                  />
+                )}
+                <Box sx={{marginTop: '16px'}}>
+                  <Button variant="contained" color="primary" type="submit">
+                    Save
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    style={{marginLeft: '16px'}}
+                    onClick={handleModalClose}
+                    // onClick={onClose}
+                  >
+                    Cancel
+                  </Button>
+                </Box>
+              </form>
+            </Box>
+          </Modal>
         </>
       )}
     </>
