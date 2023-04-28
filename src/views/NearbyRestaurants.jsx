cb0d0383478f7486e149b3df894bebe45bf982f4
Krugou <59641229+Krugou@users.noreply.github.com>
2023-04-28 15:50:59 +0300

    nearby

--- src/views/NearbyRestaurants.jsx
+++ src/views/NearbyRestaurants.jsx
@@ -8,19 +8,66 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import usePageTitle from '../hooks/UsePageTitle';
 import useScrollToTop from '../hooks/UseScrollToTop';
 
 import HeroImage from '../components/HeroImage';
+import {doFetch} from '../hooks/ApiHooks';
 import mockYelpData from '../utils/mockData';
 
 const NearbyRestaurants = () => {
   const viewText = 'Nearby Restaurants';
   useScrollToTop();
   usePageTitle(viewText);
-  const {businesses} = mockYelpData[0];
+  const [businesses, setBusinesses] = useState([]);
+  const [location, setLocation] = useState({latitude: null, longitude: null});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!location.latitude || !location.longitude) return;
+
+      try {
+        const proxyUrl = 'http://167.71.51.18:3000/yelp';
+        const yelpUrl = `/v3/businesses/search?latitude=${location.latitude}&longitude=${location.longitude}&categories=restaurants&limit=50`;
+        const response = await doFetch(proxyUrl + yelpUrl);
+
+        setBusinesses(response.businesses);
+      } catch (error) {
+        console.error('Error fetching Yelp data:', error);
+        // Use mock data when fetch fails
+        setBusinesses(mockYelpData[0].businesses);
+      }
+    };
+
+    fetchData();
+  }, [location]);
+
+  useEffect(() => {
+    const getGeolocation = () => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLocation({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          (error) => {
+            console.error('Error getting geolocation:', error);
+          },
+          {enableHighAccuracy: true, timeout: 5000, maximumAge: 0}
+        );
+      } else {
+        console.error('Geolocation not supported by this browser.');
+      }
+    };
+
+    getGeolocation();
+  }, []);
+
+  const placeholderImage = 'https://via.placeholder.com/140'; // Add a placeholder image URL
 
   return (
     <>
@@ -39,13 +86,13 @@ const NearbyRestaurants = () => {
                 <CardMedia
                   component="img"
                   height="140"
-                  image={restaurant.image_url}
+                  image={restaurant.image_url || placeholderImage} // Use a placeholder image if there's no image_url
                   alt={restaurant.name}
                 />
                 <CardContent sx={{flexGrow: 1}}>
                   <Typography variant="h5" component="div" noWrap>
                     {restaurant.name}
-                  </Typography>
+                  </Typography>{' '}
                   <Box mt={1} sx={{minHeight: '3em'}}>
                     {restaurant.categories.map((category) => (
                       <Chip
