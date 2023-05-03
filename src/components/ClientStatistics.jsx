66766e6ac8032a06e48a6b8fd435c51b171a671d
Krugou <59641229+Krugou@users.noreply.github.com>
2023-05-03 10:36:54 +0300

    targetid client stats

--- src/components/ClientStatistics.jsx
+++ src/components/ClientStatistics.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Typography, Box, Grid} from '@mui/material';
 
-const ClientStatistics = ({mediaCount, favoritesCount, otherCount}) => {
+const ClientStatistics = ({ targetId }) => {
+  const [mediaCount, setMediaCount] = useState(0);
+  const [favoritesCount, setFavoritesCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Fetch media count, favorites count, and other count for the user
+      // Replace these lines with actual API calls to fetch the data
+      setMediaCount(Math.floor(Math.random() * 100));
+      setFavoritesCount(Math.floor(Math.random() * 100));
+      setOtherCount(Math.floor(Math.random() * 100));
+    };
+
+    fetchData();
+  }, [userId]);
+
   return (
     <Grid container justifyContent="center" alignItems="center" spacing={2}>
+      {/* Media count */}
       <Grid item xs={12} sm={4}>
         <Box
           sx={{
@@ -11,13 +28,13 @@ const ClientStatistics = ({mediaCount, favoritesCount, otherCount}) => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            p: {xs: '1rem', sm: '2rem'},
+            p: { xs: '1rem', sm: '2rem' },
             borderRadius: '0.5rem',
             backgroundColor: 'primary.main',
             color: 'background.paper',
             width: '100%',
             maxWidth: '100%',
-            margin: {xs: '1rem 0', sm: '1rem'},
+            margin: { xs: '1rem 0', sm: '1rem' },
           }}
         >
           <Typography variant="h4" component="div">
@@ -26,6 +43,7 @@ const ClientStatistics = ({mediaCount, favoritesCount, otherCount}) => {
           <Typography variant="subtitle1">Media Items</Typography>
         </Box>
       </Grid>
+      {/* Favorites count */}
       <Grid item xs={12} sm={4}>
         <Box
           sx={{
@@ -33,13 +51,13 @@ const ClientStatistics = ({mediaCount, favoritesCount, otherCount}) => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            p: {xs: '1rem', sm: '2rem'},
+            p: { xs: '1rem', sm: '2rem' },
             borderRadius: '0.5rem',
             backgroundColor: 'primary.main',
             color: 'background.paper',
             width: '100%',
             maxWidth: '100%',
-            margin: {xs: '1rem 0', sm: '1rem'},
+            margin: { xs: '1rem 0', sm: '1rem' },
           }}
         >
           <Typography variant="h4" component="div">
@@ -48,6 +66,7 @@ const ClientStatistics = ({mediaCount, favoritesCount, otherCount}) => {
           <Typography variant="subtitle1">Favorites</Typography>
         </Box>
       </Grid>
+      {/* Other data */}
       <Grid item xs={12} sm={4}>
         <Box
           sx={{
@@ -55,13 +74,13 @@ const ClientStatistics = ({mediaCount, favoritesCount, otherCount}) => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            p: {xs: '1rem', sm: '2rem'},
+            p: { xs: '1rem', sm: '2rem' },
             borderRadius: '0.5rem',
             backgroundColor: 'primary.main',
             color: 'background.paper',
             width: '100%',
             maxWidth: '100%',
-            margin: {xs: '1rem 0', sm: '1rem'},
+            margin: { xs: '1rem 0', sm: '1rem' },
           }}
         >
           <Typography variant="h4" component="div">
