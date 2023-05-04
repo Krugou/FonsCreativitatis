48934214c529ebcbf0f5a11c65177295e2adcddd
Krugou <59641229+Krugou@users.noreply.github.com>
2023-05-04 10:45:16 +0300

    footer säätöä

--- src/views/PrivacyPolicy.jsx
+++ src/views/PrivacyPolicy.jsx
@@ -1,4 +1,4 @@
-import {Box, Container, Typography} from '@mui/material';
+import {Box, Container, List, ListItem, Typography} from '@mui/material';
 import React, {useEffect} from 'react';
 import HeroImage from '../components/HeroImage';
 import usePageTitle from '../hooks/UsePageTitle';
@@ -12,60 +12,71 @@ const PrivacyPolicy = () => {
     <>
       <HeroImage heroText={viewText} />
       <Container maxWidth="md">
-        <Box sx={{my: {xs: '2em', md: '3em'}, px: {xs: '1em', md: '2em'}}}>
-          <Typography variant="h4" sx={{mb: '1em'}}>
+        <Box
+          sx={{
+            my: {xs: '2em', md: '3em'},
+            px: {xs: '1em', md: '2em'},
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h4" sx={{mb: '1rem'}}>
             Privacy Policy
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             This Privacy Policy describes how JAKReviews (“we,” “us,” or “our”)
             collects, uses, and shares your personal information when you use
             our restaurant review website.
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             Information We Collect
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             We collect the following types of personal information when you use
             our website:
-            <ul>
-              <li>Your name</li>
-              <li>Your email address</li>
-              <li>Your IP address</li>
-              <li>
+            <List sx={{pl: '1.5rem'}}>
+              <ListItem>Your name</ListItem>
+              <ListItem>Your email address</ListItem>
+              <ListItem>Your IP address</ListItem>
+              <ListItem>
                 Any other information you voluntarily provide us through our
                 website, such as restaurant reviews or comments
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             How We Use Your Information
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             We use your personal information to:
-            <ul>
-              <li>Provide and improve our restaurant review website</li>
-              <li>Respond to your inquiries and comments</li>
-              <li>
+            <List sx={{pl: '1.5rem'}}>
+              <ListItem>
+                Provide and improve our restaurant review website
+              </ListItem>
+              <ListItem>Respond to your inquiries and comments</ListItem>
+              <ListItem>
                 Send you marketing communications, if you have opted-in to
                 receive them
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             How We Share Your Information
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             We may share your personal information with:
-            <ul>
-              <li>
+            <List sx={{pl: '1.5rem'}}>
+              <ListItem>
                 Third-party service providers who help us operate our website
                 and provide services to you
-              </li>
-              <li>
+              </ListItem>
+              <ListItem>
                 Law enforcement or other government officials, in response to a
                 subpoena, court order, or similar legal requirement
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Typography>
           <Typography variant="h5" sx={{mb: '1em'}}>
             Your Rights and Choices
@@ -73,35 +84,35 @@ const PrivacyPolicy = () => {
           <Typography variant="body1" sx={{mb: '1em'}}>
             You have the following rights and choices regarding your personal
             information:
-            <ul>
-              <li>
+            <List sx={{pl: '1.5rem'}}>
+              <ListItem>
                 You can access and update your personal information by logging
                 into your account on our website
-              </li>
-              <li>
+              </ListItem>
+              <ListItem>
                 You can unsubscribe from our marketing communications at any
                 time by clicking the “unsubscribe” link in the email
-              </li>
-              <li>
+              </ListItem>
+              <ListItem>
                 You can delete your account and all associated personal
                 information by contacting us at [contact email]
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             Data Retention
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             We retain your personal information for as long as necessary to
             fulfill the purposes outlined in this Privacy Policy, unless a
             longer retention period is required or permitted by law. We will
             securely delete your personal information once it is no longer
             needed for the purposes stated in this Privacy Policy.
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             Security of Your Information
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             We take reasonable steps to protect your personal information from
             unauthorized access, disclosure, or alteration. We use a variety of
             security technologies and procedures to help protect your personal
@@ -114,20 +125,20 @@ const PrivacyPolicy = () => {
             and all information and data exchanged between you and us through
             this website cannot be guaranteed.
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             Changes to Our Privacy Policy
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             We may update this Privacy Policy from time to time. If we make
             material changes to this Privacy Policy, we will notify you by
             posting a notice on our website prior to the effective date of the
             changes. By continuing to use our website after the effective date
             of any changes, you agree to be bound by the revised Privacy Policy.
           </Typography>
-          <Typography variant="h5" sx={{mb: '1em'}}>
+          <Typography variant="h5" sx={{mb: '1rem'}}>
             Contact Us
           </Typography>
-          <Typography variant="body1" sx={{mb: '1em'}}>
+          <Typography variant="body1" sx={{mb: '1rem', textAlign: 'center'}}>
             If you have any questions or concerns about this Privacy Policy,
             please contact us at [contact email].
           </Typography>
